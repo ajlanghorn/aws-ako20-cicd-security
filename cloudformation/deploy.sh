d041387c8f6dbb3d19765271410ca9e9e4230e0f
Andrew Langhorn <andrewjl@amazon.com>
2020-01-06 22:35:59 +0000

    Require IAM capability

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -4,5 +4,5 @@ if [[ -z "${AWS_DEFAULT_REGION}" ]]; then
   echo "ERROR! AWS_DEFAULT_REGION environment variable not set."
   exit 1
 else
-  aws cloudformation create-stack --stack-name ako20-`date +%s` --template-body file://template.json
+  aws cloudformation create-stack --stack-name ako20-`date +%s` --template-body file://template.json --capabilities CAPABILITY_IAM
 fi
